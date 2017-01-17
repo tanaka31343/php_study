026e78978e3352f0f1f6041382cd563c0f7ad2ba
SunaoSaimyo <sunaosaimyo@gmail.com>
2017-01-17 16:15:46 +0900

    フォームの値の受け取り

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+$val = isset($_POST['val'])?$_POST['val']:"";
+// ここでvalの値を受け取れるよ
+ ?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -5,9 +9,10 @@
     <title>PHP_Study</title>
   </head>
   <body>
-    <form action="" method="GET"> 
-      <input type="text" name="val">
+    <form action="" method="POST">
+      <input type="text" name="val" value="<?php echo $val; ?>">
       <button type="submit" name="submit" value="submit">送信</button>
     </form>
+    <?php echo $val; ?>
   </body>
 </html>
